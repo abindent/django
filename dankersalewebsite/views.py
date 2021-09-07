d0a6a4c1e4eea278f8fbe6cccce1706ff5ffb9eb
Think-With-Us <smsinchanmaitra2007@gmail.com>
2021-09-07 21:38:30 +0530

    Django-Site

---
+++ dankersalewebsite/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+     return render(request, 'index.html')
+
+def aboutus(request):
+    return render(request, 'about.html')
+def banappeal(request):
+  return render(request, 'ban-appeal.html') 
+def stats(request):
+      return render(request, 'stats.html') 
+     
+  

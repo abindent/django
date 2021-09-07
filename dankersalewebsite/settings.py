9e5972954215a8c0eb5242e2fae27aa26f0d8410
Think-With-Us <86192575+Think-With-Us@users.noreply.github.com>
2021-09-07 22:55:29 +0530

    Update settings.py

--- dankersalewebsite/settings.py
+++ dankersalewebsite/settings.py
@@ -126,6 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+#DAF
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
